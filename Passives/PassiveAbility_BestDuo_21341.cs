134a67fa8c44d56fea5931f50a6cc5eaf9c2034b
Kamiyo <kamiyo@outlook.it>
2022-04-30 18:30:02 +0200

    Add project files.

---
+++ Passives/PassiveAbility_BestDuo_21341.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlitchedDuo_21341.Passives
+{
+    public class PassiveAbility_BestDuo_21341 : PassiveAbilityBase
+    {
+    }
+}
